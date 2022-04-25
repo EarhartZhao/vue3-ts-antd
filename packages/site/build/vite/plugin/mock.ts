8b925922c48b71bd1c4235edafe2ef493eac8b6d
undefined <undefined>
2022-04-25 18:11:08 +0800

    fix: dev启动mock报错

--- packages/site/build/vite/plugin/mock.ts
+++ packages/site/build/vite/plugin/mock.ts
@@ -7,7 +7,7 @@ import { viteMockServe } from 'vite-plugin-mock';
 export function configMockPlugin(isBuild: boolean) {
   return viteMockServe({
     ignore: /^\_/,
-    mockPath: 'mock',
+    mockPath: '/packages/site/mock',
     localEnabled: !isBuild,
     prodEnabled: isBuild,
     injectCode: `
